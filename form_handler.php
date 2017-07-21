accece55fa67c5381032ca179d7ee34149c372c4
Akashdeep Ramnaney <akashdeepramnaney@gmail.com>
2017-07-22 01:21:37 +0530

    Added Forms and Startups

---
+++ form_handler.php
@@ -0,0 +1,35 @@
+<?php
+$field_name = $_POST['name'];
+$field_email = $_POST['email'];
+$field_message = $_POST['college'];
+$field_no = $_POST['phone'];
+
+$mail_to = 'iosddtu@gmail.com';
+$subject = 'Message from a site visitor '.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Phone Number: '.$field_no."\n";
+$body_message .= 'College: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+    <script language="javascript" type="text/javascript">
+        alert('Thank you we will reply back shortly.');
+        window.location = 'index.html';
+    </script>
+<?php
+}
+else { ?>
+    <script language="javascript" type="text/javascript">
+        alert('Something went wrong. Please try again or contact at iosddtu@gmail.com');
+        window.location = 'index.html';
+    </script>
+<?php
+}
+
+?>
