4126817352403140404d936cb94655153894615f
Dhruv <pythonbrilliant@gmail.com>
2017-07-21 21:05:43 +0530

    Added Form and Images

--- js/custom.js
+++ js/custom.js
@@ -1,43 +1,68 @@
-$(document).ready(function() {
-    // Test for placeholder support
-    $.support.placeholder = (function(){
-        var i = document.createElement('input');
-        return 'placeholder' in i;
-    })();
+// Auto resize input
+function resizeInput() {
+    $(this).attr('size', $(this).val().length);
+}
 
-    // Hide labels by default if placeholders are supported
-    if($.support.placeholder) {
-        $('.form-label').each(function(){
-            $(this).addClass('js-hide-label');
-        });
+$('input[type="text"], input[type="email"]')
+// event handler
+    .keyup(resizeInput)
+    // resize on page load
+    .each(resizeInput);
 
-        // Code for adding/removing classes here
-        $('.form-group').find('input, textarea').on('keyup blur focus', function(e){
 
-            // Cache our selectors
-            var $this = $(this),
-                $parent = $this.parent().find("label");
+console.clear();
+// Adapted from georgepapadakis.me/demo/expanding-textarea.html
+(function(){
 
-            if (e.type == 'keyup') {
-                if( $this.val() == '' ) {
-                    $parent.addClass('js-hide-label');
-                } else {
-                    $parent.removeClass('js-hide-label');
+    var textareas = document.querySelectorAll('.expanding'),
+
+        resize = function(t) {
+            t.style.height = 'auto';
+            t.style.overflow = 'hidden'; // Ensure scrollbar doesn't interfere with the true height of the text.
+            t.style.height = (t.scrollHeight + t.offset ) + 'px';
+            t.style.overflow = '';
+        },
+
+        attachResize = function(t) {
+            if ( t ) {
+                console.log('t.className',t.className);
+                t.offset = !window.opera ? (t.offsetHeight - t.clientHeight) : (t.offsetHeight + parseInt(window.getComputedStyle(t, null).getPropertyValue('border-top-width')));
+
+                resize(t);
+
+                if ( t.addEventListener ) {
+                    t.addEventListener('input', function() { resize(t); });
+                    t.addEventListener('mouseup', function() { resize(t); }); // set height after user resize
+                }
+
+                t['attachEvent'] && t.attachEvent('onkeyup', function() { resize(t); });
+            }
+        };
+
+    // IE7 support
+    if ( !document.querySelectorAll ) {
+
+        function getElementsByClass(searchClass,node,tag) {
+            var classElements = new Array();
+            node = node || document;
+            tag = tag || '*';
+            var els = node.getElementsByTagName(tag);
+            var elsLen = els.length;
+            var pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)");
+            for (i = 0, j = 0; i < elsLen; i++) {
+                if ( pattern.test(els[i].className) ) {
+                    classElements[j] = els[i];
+                    j++;
                 }
             }
-            else if (e.type == 'blur') {
-                if( $this.val() == '' ) {
-                    $parent.addClass('js-hide-label');
-                }
-                else {
-                    $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
-                }
-            }
-            else if (e.type == 'focus') {
-                if( $this.val() !== '' ) {
-                    $parent.removeClass('js-unhighlight-label');
-                }
-            }
-        });
+            return classElements;
+        }
+
+        textareas = getElementsByClass('expanding');
     }
-});
+
+    for (var i = 0; i < textareas.length; i++ ) {
+        attachResize(textareas[i]);
+    }
+
+})();
